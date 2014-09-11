ca5fa5e94fefd9eec7667ef61a22b0a0224eaedf
chunjunghee <dkddkd0311@naver.com>
2014-09-11 15:07:34 +0900

    04

--- sketch.js
+++ sketch.js
@@ -5,11 +5,12 @@ function setup() {
   fill(255, 0, 0);
 
    stroke(255,0,0)
-  line(10, 10, 90, 10); // x1, y1, x2, y2
+   line(10, 10, 90, 10); // x1, y1, x2, y2
 
   	fill(200, 249, 103);
   	stroke(200, 249, 103)
     rect(20, 20, 80, 50); // x, y, width, height
+
     fill(9, 112, 103);
   	stroke(9, 112, 103)
     ellipse(50, 50, 50, 50); // centerX, centerY, width, height
