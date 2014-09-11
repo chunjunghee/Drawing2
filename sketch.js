407c74b67e5e31c0418232ef628cb7da6f8c974d
chunjunghee <dkddkd0311@naver.com>
2014-09-11 14:57:44 +0900

    03

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 function setup() {
   createCanvas(640, 480);
   //background(red, green, blue);
-  background(255, 0, 0);
-  stroke(255);
-  noFill();
+  background(0, 255, 0);
+  Fill(255, 0, 0);
 
+   stroke(255,0,0)
   line(10, 10, 90, 10); // x1, y1, x2, y2
+
+  	fill(200, 249, 103);
+  	stroke(200, 249, 103)
     rect(20, 20, 80, 50); // x, y, width, height
+
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
 }
